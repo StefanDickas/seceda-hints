04cf9fdda8065006959a206df2ba770ef3e80d7c
Stefan <stefan.dickas@gmx.de>
2021-10-11 15:54:27 +0200

    rss-runtime

---
+++ rss-runtime/start-rss.bat
@@ -0,0 +1,7 @@
+REM set DB2INSTANCE=DB2
+REM set LIB=C:\Program Files\IBM\SQLLIB\lib
+REM set PATH=C:\Program Files\Amazon Corretto\jdk16.0.2_7\bin\server;%PATH%
+REM set PATH=C:\Progra~1\Java\jre1.8.0_291\bin\server;C:\Progra~1\Java\jre1.8.0_291;C:\Progra~1\Java\jre1.8.0_291\bin
+set PATH=C:\Program Files\Java\jdk1.8.0_281\bin;C:\Program Files\Java\jdk1.8.0_281\jre\bin;C:\Program Files\Java\jdk1.8.0_281\jre\bin\server
+set JAVA_HOME=C:\Progra~1\Java\jre1.8.0_291
+rss.exe --config math.ini --workers 1
