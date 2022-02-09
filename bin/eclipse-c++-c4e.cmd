ee0f4a26bcdb92a9f719c26682de52963ebc81e9
stefan <stefan@host.docker.internal>
2022-02-09 13:26:20 +0100

    some hints

---
+++ bin/eclipse-c++-c4e.cmd
@@ -0,0 +1,3 @@
+start C:\Progra~2\Micros~2\2017\Professional\Common7\Tools\VsDevCmd.bat
+
+C:\Users\stefan\eclipse\cpp-2021-09\eclipse\eclipse.exe -data C:\Users\stefan\eclipse-workspace\seceda-cplusplus-nmake -showlocation c++-seceda-nmake
