cf9bea2074ecdf69af3a362cafc5aefdb4e94316
stefan <stefan@host.docker.internal>
2022-02-11 16:27:28 +0100

    eclipse mit nmake cmake

--- bin/eclipse-c++-c4e.cmd
+++ bin/eclipse-c++-c4e.cmd
@@ -1,3 +1,25 @@
-start C:\Progra~2\Micros~2\2017\Professional\Common7\Tools\VsDevCmd.bat
+set PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;
+set PATH=C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Git\cmd;%PATH%
+REM set PATH=C:\Program Files\Microsoft VS Code\bin;%PATH%
+REM set PATH=C:\Strawberry\c\bin;C:\Strawberry\perl\site\bin;;%PATH%
+set PATH=C:\Strawberry\perl\bin;%PATH%
+REM set PATH=C:\Program Files\dotnet\;C:\Program Files\Microsoft SQL Server\130\Tools\Bin\;%PATH%
+set PATH=C:\Program Files\cmake\cmake-3.17.2-win64-x64\bin;%PATH%
+set PATH=C:\Program Files\NASM;%PATH%
+REM set PATH=C:\Users\stefan\AppData\Local\Programs\Python\Python39\python.exe;%PATH%
+set PATH=C:\Users\stefan\AppData\Local\Microsoft\WindowsApps;%PATH%
+set PATH=C:\Users\stefan\.dotnet\tools;%PATH%
 
-C:\Users\stefan\eclipse\cpp-2021-09\eclipse\eclipse.exe -data C:\Users\stefan\eclipse-workspace\seceda-cplusplus-nmake -showlocation c++-seceda-nmake
+REM ==== JAVA ====
+
+REM set PATH=C:\Program Files\Amazon Corretto\jdk16.0.2_7\bin;
+REM set PATH=C:\Program Files (x86)\Common Files\Oracle\Java\javapath;
+
+set PATH=C:\Program Files\Java\jdk1.8.0_281\bin;C:\Program Files\Java\jdk1.8.0_281\jre\bin;C:\Program Files\Java\jdk1.8.0_281\jre\bin\server;%PATH%
+set JAVA_HOME=C:\Progra~1\Java\jre1.8.0_291
+
+
+
+REM start C:\Progra~2\Micros~2\2017\Professional\Common7\Tools\VsDevCmd.bat
+REM %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat
+%comspec% /k "C:\Progra~2\Micros~2\2017\Professional\VC\Auxiliary\Build\vcvars64.bat && C:\Users\stefan\eclipse\cpp-2021-09\eclipse\eclipse.exe -data C:\Users\stefan\eclipse-workspace\seceda-cplusplus-c4e -showlocation c++-seceda-c4e
--- bin/eclipse-c++-nmake.cmd
+++ bin/eclipse-c++-nmake.cmd
@@ -1,3 +1,3 @@
 C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat
 
-C:\Users\stefan\eclipse\cpp-2021-09\eclipse\eclipse.exe -data C:\Users\stefan\eclipse-workspace\seceda-cplusplus-c4e -showlocation c++-seceda-nmake
+C:\Users\stefan\eclipse\cpp-2021-09\eclipse\eclipse.exe -data C:\Users\stefan\eclipse-workspace\seceda-cplusplus-nmake -showlocation c++-seceda-nmake
